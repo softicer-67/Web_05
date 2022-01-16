646fa05a615074da090be0be7314ffaa1e99b3cd
Yuri <9022368@mail.ru>
2022-01-16 23:51:17 +0500

    Lesson_8

--- todoapp/tests.py
+++ todoapp/tests.py
@@ -23,8 +23,3 @@ class TestTodoapp(TestCase):
         client.force_authenticate(user=admin)
         response = client.get('/api/users/')
         self.assertEqual(response.status_code, status.HTTP_200_OK)
-
-    def test_create_project(self):
-        factory = APITestCase()
-        factory = APIRequestFactory()
-        request = factory.post('/name/', {'title': 'new idea'})
